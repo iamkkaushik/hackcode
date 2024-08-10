25e3b2ec1d8718102aacfb215080973416fbf697
HarshDDalwadi <102744867+HarshDDalwadi@users.noreply.github.com>
2024-08-11 00:36:53 +0530

    Added errors to the playground

---
+++ hackex-compiler/utils/executionCodes/afbc7f10-9544-48e6-b725-7170ddabcaa5.cpp
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // This is a single-line comment
+        System.out.println("Hello, World!"); // This line prints "Hello, World!" to the console
+    }
+}
